d61d42448a93c1a025acdfcb265be7fff70935c5
Jackson-P-Tesla <jackson.c.parker@gmail.com>
2024-09-13 00:17:07 -0400

    chore: hopefully final fix for the hirdmann site

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,28 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Form validation and spam prevention
-    const contactForm = document.getElementById('contactForm');
-    if (contactForm) {
-        contactForm.addEventListener('submit', function(event) {
-            const email = document.getElementById('email').value;
-            const honeypot = document.getElementById('company').value;
-
-            // Check honeypot
-            if (honeypot) {
-                event.preventDefault(); // Prevent form submission if honeypot is filled
-                alert("Spam detected!");
-            }
-
-            // Basic email format validation
-            const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            if (!emailPattern.test(email)) {
-                event.preventDefault();
-                alert("Please enter a valid email address.");
-            }
-        });
-    } else {
-        console.error("Contact form not found");
-    }
-
     // Hamburger navigation logic
     const hamburger = document.getElementById('hamburger');
     const mobileNav = document.getElementById('mobile-nav');
